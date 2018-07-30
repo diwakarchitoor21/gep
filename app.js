2d5bc2342ce7123b1009b6cf849b8dc25ff93a7a
diwakarchitoor21 <diwakarchitoor21@gmail.com>
2018-07-30 02:36:44 +0000

    Edited from World to word

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send('Hello word');
 });
 app.listen(3000, function () {
-  console.log('Hello World app listening on port 3000!');
+  console.log('Hello word app listening on port 3000!');
 });
